66df55877a7295660338b824ae9a52175cf54faa
halid <you@example.com>
2024-10-23 13:03:05 +0800

    updet ni rek

---
+++ Jurnal/index.js
@@ -0,0 +1,14 @@
+
+let halid = 2;
+let riski = 3;
+function tambah(satu, dua) {
+    satu = halid;
+    dua = riski;
+    let hasil = satu + dua;
+  return hasil; 
+}
+
+function tampil() {
+    console.log(tambah());
+    
+}
